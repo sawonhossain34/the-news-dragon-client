a303ec774223bacb98a603c412f962111f26f310
sawonhossain34 <sawonhossain3491@gmail.com>
2023-04-18 23:59:40 +0600

    react marquee and responsive navbar react bootstrap

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
+import { Col, Container, Row } from 'react-bootstrap';
+import RightNav from '../pages/Shared/RightNav/RightNav';
+import StartNav from '../pages/Shared/StartNav/StartNav';
+
+
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
 
-            
+            <Container>
+                <Row>
+                    <Col lg={3}>
+                        <StartNav></StartNav>
+                    </Col>
+                    <Col lg={6}><h2>Main contant</h2></Col>
+                    <Col lg={3}>
+                        <RightNav></RightNav>
+                    </Col>
+                </Row>
+            </Container>
             <Footer></Footer>
         </div>
     );
---
+++ src/pages/Shared/StartNav/StartNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StartNav = () => {
+    return (
+        <div>
+            <h2>Left Nav</h2>
+        </div>
+    );
+};
+
+export default StartNav;
