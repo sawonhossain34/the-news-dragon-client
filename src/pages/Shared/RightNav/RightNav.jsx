e52ff278d7e65fb5d58a70132e5eed37ac6db247
sawonhossain34 <sawonhossain3491@gmail.com>
2023-04-19 09:31:47 +0600

    right side nav all section create

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub,FaFacebook,FaTwitter,FaInstagram } from 'react-icons/fa';
+import bg from '../../../assets/bg.png'
+import QZone from '../QZone/QZone';
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right Nav</h2>
+            <h4>Login With</h4>
+            <Button className='mb-2' variant="primary">  <FaGoogle /> Login with Google</Button>
+            <Button variant="secondary"> <FaGithub /> Login with Github</Button>
+            <div>
+                <h4>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
