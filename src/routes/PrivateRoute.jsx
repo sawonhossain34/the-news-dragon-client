e2c850698d542e7f12ff12be5868f3dd8df2d72d
sawonhossain34 <sawonhossain3491@gmail.com>
2023-04-29 10:52:08 +0600

    private route and navigate location state set

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,26 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
 
-const PrivateRoute = () => {
+const PrivateRoute = ({children}) => {
     const {user} = useContext(AuthContext);
-    return (
-        <div>
-            
-        </div>
-    );
+    const location = useLocation();
+    console.log(location);
+    if(user){
+        return children;
+    }
+    return <Navigate state={{from:location}} to='/login'  replace></Navigate>
 };
 
 export default PrivateRoute;
+
+
+
+/**
+ * STEP///////
+ * 
+ * 1 cheack user is logged in or not.
+ * 2 if user is loggged in,then allow them to visit route
+ * 3 Else redirects the user to the login page
+ * 4 setup the private router
+ */
